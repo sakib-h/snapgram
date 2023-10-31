6cf0b12af4ac7d09bf0da0d8ffcc5e790ee6ad3d
Sakib Hasan <sakib100.sa@gmail.com>
2023-10-31 12:12:00 +0600

    setting up react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import "./globals.css";
+import SignInForm from "./_auth/forms/SignInForm";
+import { Home } from "./_root/pages";
 const App = () => {
-    return <h1 className="text-3xl font-bold underline">Hello Snapgram!</h1>;
+    return (
+        <main className="flex h-screen">
+            <Routes>
+                {/* Public Routes */}
+                <Route path="/sign-in" element={<SignInForm />} />
+                {/* Private Routes */}
+                <Route index element={<Home />} />
+            </Routes>
+        </main>
+    );
 };
 
 export default App;
