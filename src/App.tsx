fcdd9efd8df898349d9bc20342939f4afc300f71
Sakib Hasan <sakib100.sa@gmail.com>
2023-11-01 12:12:00 +0600

    some typo fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import "./globals.css";
 import SignInForm from "./_auth/forms/SignInForm";
 import { Home } from "./_root/pages";
+import SignUpForm from "./_auth/forms/SignUpForm";
 const App = () => {
     return (
         <main className="flex h-screen">
             <Routes>
                 {/* Public Routes */}
                 <Route path="/sign-in" element={<SignInForm />} />
+                <Route path="/sign-up" element={<SignUpForm />} />
                 {/* Private Routes */}
                 <Route index element={<Home />} />
             </Routes>
