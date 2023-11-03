a5502a55a3885f61e704d0e1a09fb9e788969416
Sakib Hasan <sakib100.sa@gmail.com>
2023-11-03 11:39:29 +0600

    implementing signup form

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -1,6 +1,24 @@
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
 
 const SignUpForm = () => {
+    const formSchema = z.object({
+        username: z.string().min(2).max(50),
+    });
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+        },
+    });
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values);
+    }
     return (
         <div>
             <Button>Click Me</Button>
