93797208b14ccce5aab488999d87bf9243ae9c7d
Sakib Hasan <sakib100.sa@gmail.com>
2023-11-12 00:00:08 +0600

    working on navbar

--- src/components/shared/TopBar.tsx
+++ src/components/shared/TopBar.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
 
 const TopBar = () => {
-    return <div>Topbar</div>;
+    return (
+        <section className="topbar">
+            <div className="flex-between py-4 px-5">
+                <Link to="/" className="flex gap-3 items-center">
+                    <img
+                        src="/assets/images/logo.svg"
+                        alt="logo"
+                        width={130}
+                        height={325}
+                    />
+                </Link>
+                <div className="flex gap-4">
+                    <Button variant="ghost">
+                        <img src="/assets/icons/logout.svg" alt="logout" />
+                    </Button>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default TopBar;
