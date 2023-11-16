790189d187bb4897d8477dcf4acd077b8dcf0bc8
Sakib Hasan <sakib100.sa@gmail.com>
2023-11-16 23:53:20 +0600

    working on postcard

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,8 +1,14 @@
 import Loader from "@/components/shared/Loader";
+import PostCard from "@/components/shared/PostCard";
+import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutations";
+import { Models } from "appwrite";
 
 const Home = () => {
-    const isPostLoading = true;
-    const posts = null;
+    const {
+        data: posts,
+        isPending: isPostLoading,
+        isError: isErrorPosts,
+    } = useGetRecentPosts();
     return (
         <div className="flex flex-1">
             <div className="home-container">
@@ -10,7 +16,15 @@ const Home = () => {
                     <h2 className="h3-bold md:h2-bold text-left w-full">
                         Home Feed
                     </h2>
-                    {isPostLoading && !posts ? <Loader /> : <ul>Posts</ul>}
+                    {isPostLoading && !posts ? (
+                        <Loader />
+                    ) : (
+                        <ul className="flex flex-col flex-1 gap-9 w-full">
+                            {posts?.documents.map((post: Models.Document) => (
+                                <PostCard post={post} />
+                            ))}
+                        </ul>
+                    )}
                 </div>
             </div>
         </div>
