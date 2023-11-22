db9de54c672b6e65ac0c6f7d19ed90bd5ec00b32
Sakib Hasan <sakib100.sa@gmail.com>
2023-11-22 11:27:51 +0600

    working on explore pagge

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,4 +1,7 @@
 import Loader from "@/components/shared/Loader";
+import PostStats from "@/components/shared/PostStats";
+import { Button } from "@/components/ui/button";
+import { useUserContext } from "@/context/AuthContext";
 import { useGetPostById } from "@/lib/react-query/queriesAndMutations";
 import { formatDateString } from "@/lib/utils";
 import { Link, useParams } from "react-router-dom";
@@ -6,6 +9,8 @@ import { Link, useParams } from "react-router-dom";
 const PostDetails = () => {
     const { id } = useParams();
     const { data: post, isPending } = useGetPostById(id || "");
+    const { user } = useUserContext();
+    const handleDeletePost = () => {};
     return (
         <div className="post_details-container">
             {isPending ? (
@@ -29,7 +34,7 @@ const PostDetails = () => {
                                         "/assets/icons/profile-placeholder.svg"
                                     }
                                     alt="creator"
-                                    className="rounded-full w-12 lg:h-12"
+                                    className="rounded-full w-8 h-8  lg:w-12 lg:h-12"
                                 />
 
                                 <div className="flex flex-col ">
@@ -49,11 +54,54 @@ const PostDetails = () => {
                                     </div>
                                 </div>
                             </Link>
-                            <div className="flex-center gap-4">
-                                <Link to={`/update-post/${post?.$id}`}>
-                                    <img src="/assets/icons/edit.svg" alt="save" />
+                            <div className="flex-center ">
+                                <Link
+                                    to={`/update-post/${post?.$id}`}
+                                    className={`${
+                                        user.id !== post?.creator.$id &&
+                                        "hidden"
+                                    }`}
+                                >
+                                    <img
+                                        src="/assets/icons/edit.svg"
+                                        alt="edit"
+                                        width={24}
+                                        height={24}
+                                    />
                                 </Link>
+                                <Button
+                                    onClick={handleDeletePost}
+                                    variant="ghost"
+                                    className={`${
+                                        user.id !== post?.creator.$id &&
+                                        "hidden"
+                                    } ghost_details-delete_btn`}
+                                >
+                                    <img
+                                        src="/assets/icons/delete.svg"
+                                        alt="delete"
+                                        width={24}
+                                        height={24}
+                                    />
+                                </Button>
                             </div>
+                        </div>
+
+                        <hr className="border w-full border-dark-4/80" />
+
+                        <div className="flex flex-col flex-1 w-full small-medium lg:base-regular">
+                            <p>{post?.caption}</p>
+                            <ul className="flex gap-1 mt-2">
+                                {post?.tags.map((tag: string) => (
+                                    <li key={tag} className="text-light-3">
+                                        #{tag}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+
+                        <div className="w-full">
+                            <PostStats post={post} userId={user.id}/>
                         </div>
                     </div>
                 </div>
--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -10,12 +10,12 @@ import React, { useState, useEffect } from "react";
 import Loader from "./Loader";
 
 type PostStatsProps = {
-    post: Models.Document;
+    post?: Models.Document;
     userId: string;
 };
 
 const PostStats = ({ post, userId }: PostStatsProps) => {
-    const likesList = post.likes.map((user: Models.Document) => user.$id);
+    const likesList = post?.likes.map((user: Models.Document) => user.$id);
     // states
     const [likes, setLikes] = useState(likesList);
     const [isSaved, setIsSaved] = useState(false);
@@ -28,7 +28,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
     const { data: currentUser } = useGetCurrentUser();
 
     const savedPostRecord = currentUser?.save.find(
-        (record: Models.Document) => record.post.$id === post.$id
+        (record: Models.Document) => record.post.$id === post?.$id
     );
 
     useEffect(() => {
@@ -46,7 +46,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
             newLikes.push(userId);
         }
         setLikes(newLikes);
-        likePost({ postId: post.$id, likesArray: newLikes });
+        likePost({ postId: post?.$id || "", likesArray: newLikes });
     };
     const handleSavePost = (e: React.MouseEvent) => {
         e.stopPropagation();
@@ -55,7 +55,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
             setIsSaved(false);
             deleteSavedPost(savedPostRecord.$id);
         } else {
-            savePost({ postId: post.$id, userId });
+            savePost({ postId: post?.$id || "", userId });
             setIsSaved(true);
         }
     };
