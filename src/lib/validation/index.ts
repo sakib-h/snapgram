88d2c3b351e352ffc36e4906fb6ab6da8434014f
Sakib Hasan <sakib100.sa@gmail.com>
2024-11-04 12:12:00 +0600

    signup validation added

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -3,7 +3,14 @@ import * as z from "zod";
 export const signUpValidation = z.object({
     name: z
         .string()
-        .min(2, { message: "Name must be at least 2 characters long" })
+        .min(2, { message: "Name must be at least 3 characters long" })
         .max(50, { message: "Name must be at most 50 characters long" }),
-    username: z.string().min(2).max(50),
+    username: z
+        .string()
+        .min(2, { message: "Name must be at least 3 characters long" }),
+
+    email: z.string().email({ message: "Invalid email" }),
+    password: z
+        .string()
+        .min(8, { message: "Password must be at least 8 characters long" }),
 });
